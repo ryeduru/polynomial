0d7c5fd2e84a54dc6935f2766cd0f66bfed6cf2f
Ritvik Yeduru <ryeduru@bu.edu>
2024-01-27 21:26:30 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -19,6 +19,23 @@ class Add:
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        return repr(self.p1) + " - " + repr(self.p2)
+
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        return repr(self.p1) + " / " + repr(self.p2)
+
 
 class Mul:
     def __init__(self, p1, p2):
@@ -26,13 +43,16 @@ class Mul:
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+        if isinstance(self.p1, (Add, Sub, Div)):
+            left_repr = repr(self.p1)
+        else:
+            left_repr = repr(self.p1)
+
+        if isinstance(self.p2, (Add, Sub, Div)):
+            right_repr = repr(self.p2)
+        else:
+            right_repr = repr(self.p2)
+        return left_repr + " * " + right_repr
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
